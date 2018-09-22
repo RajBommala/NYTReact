7b1f3794ef7e6ea07cc7db19e8a26c5c5a937e9c
Bommala <basusmy@gmail.com>
2018-09-21 22:09:06 -0500

    initial commit

---
+++ models/article.js
@@ -0,0 +1,16 @@
+
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const articleSchema = new Schema({
+  title: {type: String, require: true},
+  image: {type: String, require: true},
+  date: { type: Date, default: Date.now },
+  url: {type: String, require: true},
+  articleId: {type: String, require: true},
+});
+
+const Article = mongoose.model("Article", articleSchema); // This creates our model from the above schema, using mongoose's model method
+
+module.exports = Article;
---
+++ server.js
@@ -0,0 +1,48 @@
+
+const express = require('express'); // Server
+const bodyParser = require ('body-parser'); // JSON Middleware
+const logger = require('morgan'); // REST Logger
+const mongoose = require('mongoose'); // MongoDB ORM
+const routes = require("./routes");
+let db = require("./models"); // Require all models
+
+
+let PORT = process.env.PORT || 3030;
+let mongooseConnection = mongoose.connection;
+
+
+let app = express();
+
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true})); // Allows For JSON Interactions Between Client & Server
+app.use(express.static("client/build")); // Serve Static React Pages
+app.use(bodyParser.text());
+app.use(bodyParser.json({type: "application/vnd.api+json"}));
+
+mongoose.Promise = global.Promise; // Set up promises with mongoose
+
+// Connect to the Mongo DB
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/NYTReact",
+{ useNewUrlParser: true } 
+);
+
+mongooseConnection.on('error', console.error.bind(console, 'connection error:'));
+
+mongooseConnection.once('open', function() {
+  console.log(`Sucessfully Connected to Mongo DB !`); // If Connection is successful, Console.log(Message)
+});
+
+var cors = require("cors");
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+app.use(cors());
+
+app.use(routes); // Add routes, both API and View
+
+app.listen(PORT, function() {
+  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+});
